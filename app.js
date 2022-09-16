a56d3f92125bfa98be23912a9d7e98380e9cd747
jvjsfun <juanvalderrama.work@gmail.com>
2022-09-17 01:50:43 +0500

    json file read from node server.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 var jsonData = require("./data.json");
+var cors = require('cors')
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
